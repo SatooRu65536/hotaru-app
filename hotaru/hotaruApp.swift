7b0f2c72933f874c5ddffd7555f5a85a42700be0
SatooRu65536 <ryuto4619@gmail.com>
2023-07-17 14:16:20 +0900

    Initial Commit

---
+++ hotaru/hotaruApp.swift
@@ -0,0 +1,17 @@
+//
+//  hotaruApp.swift
+//  hotaru
+//
+//  Created by 多田隆人 on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct hotaruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
